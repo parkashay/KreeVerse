197d65056992b2ab6e1884887d9421768dcba476
leviasthur <prajjwalt9@gmail.com>
2023-11-17 15:39:00 +0545

    feat: changed product details statically and gallery

--- app/routes/products/$slug.tsx
+++ app/routes/products/$slug.tsx
@@ -1,5 +1,5 @@
 import { DataFunctionArgs, json } from '@remix-run/server-runtime';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Price } from '~/components/products/Price';
 import { getProductBySlug } from '~/providers/products/products';
 import {
@@ -19,18 +19,45 @@ import Alert from '~/components/Alert';
 import { StockLevelLabel } from '~/components/products/StockLevelLabel';
 import TopReviews from '~/components/products/TopReviews';
 import { ScrollableContainer } from '~/components/products/ScrollableContainer';
+// import {
+//   SfButton,
+//   SfIconPackage,
+//   SfIconShoppingCart,
+//   SfIconUnfoldMore,
+// } from '@storefront-ui/react';
 import {
-  SfButton,
-  SfIconAddShoppingCart,
+  SfSelect,
   SfIconCheckCircle,
   SfIconCreditCard,
-  SfIconPackage,
+  SfIconAddShoppingCart,
+  SfRating,
+  SfButton,
+  SfLink,
+  SfCounter,
   SfIconShoppingCart,
+  SfIconCompareArrows,
+  SfIconFavorite,
+  SfIconSell,
+  SfIconPackage,
+  SfIconRemove,
+  SfIconAdd,
+  SfIconWarehouse,
+  SfIconSafetyCheck,
   SfIconShoppingCartCheckout,
-  SfIconUnfoldMore,
-  SfSelect,
 } from '@storefront-ui/react';
-
+import { useCounter } from 'react-use';
+import { useId, ChangeEvent } from 'react';
+import { clamp } from '@storefront-ui/shared';
+import { adjustOrderLine } from '~/providers/orders/order';
+import { useIntersection } from 'react-use';
+import {
+  SfScrollable,
+  SfIconChevronLeft,
+  SfIconChevronRight,
+  type SfScrollableOnDragEndData,
+} from '@storefront-ui/react';
+import classNames from 'classnames';
+import GalleryVertical from '~/components/products/Gallery';
 export const meta: MetaFunction = ({ data }) => {
   return [
     {
@@ -65,6 +92,15 @@ export async function loader({ params, request }: DataFunctionArgs) {
 export const shoudRevalidate: ShouldRevalidateFunction = () => true;
 
 export default function ProductSlug() {
+  const inputId = useId();
+  const min = 1;
+  const max = 999;
+  const [value, { inc, dec, set }] = useCounter(min);
+  function handleOnChange(event: ChangeEvent<HTMLInputElement>) {
+    const { value: currentValue } = event.target;
+    const nextValue = parseFloat(currentValue);
+    set(Number(clamp(nextValue, min, max)));
+  }
   const { product, error } = useLoaderData<typeof loader>();
   const { activeOrderFetcher } = useOutletContext<{
     activeOrderFetcher: FetcherWithComponents<CartLoaderData>;
@@ -100,6 +136,30 @@ export default function ProductSlug() {
     selectedVariant?.featuredAsset,
   );
 
+  const lastThumbRef = useRef<HTMLButtonElement>(null);
+  const thumbsRef = useRef<HTMLDivElement>(null);
+  const firstThumbRef = useRef<HTMLButtonElement>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const firstThumbVisible = useIntersection(firstThumbRef, {
+    root: thumbsRef.current,
+    rootMargin: '0px',
+    threshold: 1,
+  });
+
+  const lastThumbVisible = useIntersection(lastThumbRef, {
+    root: thumbsRef.current,
+    rootMargin: '0px',
+    threshold: 1,
+  });
+
+  const onDragged = (event: SfScrollableOnDragEndData) => {
+    if (event.swipeRight && activeIndex > 0) {
+      setActiveIndex((currentActiveIndex) => currentActiveIndex - 1);
+    } else if (event.swipeLeft && activeIndex < product.assets.length - 1) {
+      setActiveIndex((currentActiveIndex) => currentActiveIndex + 1);
+    }
+  };
   return (
     <div>
       <div className="max-w-6xl mx-auto px-4">
@@ -114,49 +174,10 @@ export default function ProductSlug() {
         ></Breadcrumbs>
         <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start mt-4 md:mt-12">
           {/* Image gallery */}
-          <div className="w-full max-w-2xl mx-auto sm:block lg:max-w-none">
-            <span className="rounded-md overflow-hidden">
-              <div className="w-full h-full object-center object-cover rounded-lg">
-                <img
-                  src={
-                    (featuredAsset?.preview || product.featuredAsset?.preview) +
-                    '?w=800'
-                  }
-                  alt={product.name}
-                  className="w-full h-full object-center object-cover"
-                />
-              </div>
-            </span>
-
-            {product.assets.length > 1 && (
-              <ScrollableContainer>
-                {product.assets.map((asset) => (
-                  <div
-                    className={`basis-1/3 md:basis-1/4 flex-shrink-0 select-none touch-pan-x rounded-lg ${
-                      featuredAsset?.id == asset.id
-                        ? 'outline outline-2 outline-primary outline-offset-[-2px]'
-                        : ''
-                    }`}
-                    onClick={() => {
-                      setFeaturedAsset(asset);
-                    }}
-                  >
-                    <img
-                      draggable="false"
-                      className="rounded-lg select-none h-24 w-full object-cover"
-                      src={
-                        asset.preview +
-                        '?preset=full' /* not ideal, but technically prevents loading 2 seperate images */
-                      }
-                    />
-                  </div>
-                ))}
-              </ScrollableContainer>
-            )}
-          </div>
+          <GalleryVertical featuredAsset={product.assets} alt={product.name} />
 
           {/* Product info */}
-          <div className="mt-10 px-4 sm:px-0 sm:mt-16 lg:mt-0">
+          {/* <div className="mt-10 px-4 sm:px-0 sm:mt-16 lg:mt-0">
             <div className="">
               <h3 className="sr-only">Description</h3>
               <h2 className="mb-1 font-bold text-2xl"> {product.name} </h2>
@@ -273,7 +294,194 @@ export default function ProductSlug() {
                 </div>
               </section>
             </activeOrderFetcher.Form>
-          </div>
+          </div> */}
+          <section className="md:max-w-[640px]">
+            <div className="inline-flex items-center justify-center text-sm font-medium text-white bg-secondary-600 py-1.5 px-3 mb-4">
+              <SfIconSell size="sm" className="mr-1.5" />
+              Sale
+            </div>
+            <h1 className="mb-1 font-bold typography-headline-4 capitalize">
+              {product.name}
+            </h1>
+            <strong className="block font-bold typography-headline-3">
+              <Price
+                priceWithTax={selectedVariant?.priceWithTax}
+                currencyCode={selectedVariant?.currencyCode}
+              ></Price>
+            </strong>
+            <div className="inline-flex items-center mt-4 mb-2">
+              <SfRating size="xs" value={3} max={5} />
+              <SfCounter className="ml-1" size="xs">
+                123
+              </SfCounter>
+              <SfLink
+                href="#"
+                variant="secondary"
+                className="ml-2 text-xs text-neutral-500"
+              >
+                123 reviews
+              </SfLink>
+            </div>
+            <div
+              className="text-base text-gray-700"
+              dangerouslySetInnerHTML={{
+                __html: product.description,
+              }}
+            />
+            {/* <ul className="mb-4 font-normal typography-text-sm">
+              <li>HD Pictures & Videos and FPV Function</li>
+              <li>Intelligent Voice Control</li>
+              <li>Multiple Fun Flights</li>
+              <li>Easy to Use</li>
+              <li>Foldable Design & Double Flight Time</li>
+            </ul> */}
+            <activeOrderFetcher.Form method="post" action="/api/active-order">
+              <input type="hidden" name="action" value="addItemToOrder" />
+              {1 < product.variants.length ? (
+                <div className="mt-4">
+                  <label>
+                    <SfSelect
+                      className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm rounded-md"
+                      id="productVariant"
+                      value={selectedVariantId}
+                      name="variantId"
+                      onChange={(e) => {
+                        setSelectedVariantId(e.target.value);
+
+                        const variant = findVariantById(e.target.value);
+                        if (variant) {
+                          setFeaturedAsset(variant!.featuredAsset);
+                        }
+                      }}
+                    >
+                      {product.variants.map((variant) => (
+                        <option key={variant.id} value={variant.id}>
+                          {variant.name}
+                        </option>
+                      ))}
+                    </SfSelect>
+                  </label>
+                </div>
+              ) : (
+                <input
+                  type="hidden"
+                  name="variantId"
+                  value={selectedVariantId}
+                ></input>
+              )}
+            </activeOrderFetcher.Form>
+            <div className="py-4 mb-4 border-gray-200 border-y">
+              <div className="bg-primary-100 text-primary-700 flex justify-center gap-1.5 py-1.5 typography-text-sm items-center mb-4 rounded-md">
+                <SfIconShoppingCartCheckout />
+                {qtyInCart} in cart
+              </div>
+              <div className="items-start xs:flex">
+                <div className="flex flex-col items-stretch xs:items-center xs:inline-flex">
+                  <div className="flex border border-neutral-300 rounded-md">
+                    <SfButton
+                      variant="tertiary"
+                      square
+                      className="rounded-r-none p-3"
+                      disabled={value <= min}
+                      aria-controls={inputId}
+                      aria-label="Decrease value"
+                      onClick={() => {
+                        dec();
+                        // adjustOrderLine &&
+                        //   adjustOrderLine(product.id, value - 1);
+                      }}
+                    >
+                      <SfIconRemove />
+                    </SfButton>
+                    <input
+                      id={inputId}
+                      type="number"
+                      role="spinbutton"
+                      className="grow appearance-none mx-2 w-8 text-center bg-transparent font-medium [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-inner-spin-button]:display-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-outer-spin-button]:display-none [&::-webkit-outer-spin-button]:m-0 [-moz-appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none disabled:placeholder-disabled-900 focus-visible:outline focus-visible:outline-offset focus-visible:rounded-sm"
+                      min={min}
+                      max={max}
+                      value={value}
+                      onChange={handleOnChange}
+                    />
+                    <SfButton
+                      variant="tertiary"
+                      square
+                      className="rounded-l-none p-3"
+                      disabled={value >= max}
+                      aria-controls={inputId}
+                      aria-label="Increase value"
+                      onClick={() => inc()}
+                    >
+                      <SfIconAdd />
+                    </SfButton>
+                  </div>
+                  <p className="self-center mt-1 mb-4 text-xs text-neutral-500 xs:mb-0">
+                    <strong className="text-neutral-900">{max}</strong> in stock
+                  </p>
+                </div>
+                <SfButton
+                  size="lg"
+                  className="w-full xs:ml-4"
+                  slotPrefix={<SfIconShoppingCart size="sm" />}
+                >
+                  Add to cart
+                </SfButton>
+              </div>
+              <div className="flex justify-center mt-4 gap-x-4">
+                <SfButton
+                  size="sm"
+                  variant="tertiary"
+                  slotPrefix={<SfIconCompareArrows size="sm" />}
+                >
+                  Compare
+                </SfButton>
+                <SfButton
+                  size="sm"
+                  variant="tertiary"
+                  slotPrefix={<SfIconFavorite size="sm" />}
+                >
+                  Add to list
+                </SfButton>
+              </div>
+            </div>
+            <div className="flex first:mt-4">
+              <SfIconPackage
+                size="sm"
+                className="flex-shrink-0 mr-1 text-neutral-500"
+              />
+              <p className="text-sm">
+                Free shipping, arrives by Thu, Apr 7. Want it faster?
+                <SfLink href="#" variant="secondary" className="mx-1">
+                  Add an address
+                </SfLink>
+                to see options
+              </p>
+            </div>
+            <div className="flex mt-4">
+              <SfIconWarehouse
+                size="sm"
+                className="flex-shrink-0 mr-1 text-neutral-500"
+              />
+              <p className="text-sm">
+                Pickup not available at your shop.
+                <SfLink href="#" variant="secondary" className="ml-1">
+                  Check availability nearby
+                </SfLink>
+              </p>
+            </div>
+            <div className="flex mt-4">
+              <SfIconSafetyCheck
+                size="sm"
+                className="flex-shrink-0 mr-1 text-neutral-500"
+              />
+              <p className="text-sm">
+                Free 30-days returns.
+                <SfLink href="#" variant="secondary" className="ml-1">
+                  Details
+                </SfLink>
+              </p>
+            </div>
+          </section>
         </div>
       </div>
       <div className="mt-24">
