b934e743b703bb07aa73c6c0cb6d38e1e1b987f6
Prakash Poudel <prakaas,poudell@gmail.com>
2023-11-27 19:01:29 +0545

    a

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,5 @@
 export const APP_META_TITLE = 'KreeVerse - A shop for you';
 export const APP_META_DESCRIPTION =
   'A headless commerce storefront starter kit built with Remix & Vendure';
-export const DEMO_API_URL = 'https://readonlydemo.vendure.io/shop-api';
+// export const DEMO_API_URL = 'https://readonlydemo.vendure.io/shop-api';
+export const DEMO_API_URL = 'https://pcadmin.chuggerpumps.com/shop-api';
