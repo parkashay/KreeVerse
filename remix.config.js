5680a4168b10f30fd81e68a0ecc75ee49361b7a7
Prakash Poudel <prakaas,poudell@gmail.com>
2023-11-27 18:30:28 +0545

    update

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,57 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
+const { createRoutesFromFolders } = require('@remix-run/v1-route-convention');
+
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+const cloudflarePagesConfig = {
+  serverBuildTarget: 'cloudflare-pages',
+  server: './server-cloudflare-pages.js',
   ignoredRouteFiles: ['**/.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
 };
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+const netlifyConfig = {
+  serverBuildTarget: 'netlify',
+  server: './server-netlify.js',
+  ignoredRouteFiles: ['**/.*'],
+};
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+const devConfig = {
+  appDirectory: 'app',
+  serverModuleFormat: 'cjs',
+  devServerPort: 8002,
+  ignoredRouteFiles: ['.*'],
+  future: {
+    v2_dev: true,
+  },
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes);
+  },
+};
+
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+const buildConfig = {
+  appDirectory: 'app',
+  assetsBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  serverBuildDirectory: 'build',
+  ignoredRouteFiles: ['.*'],
+};
+
+function selectConfig() {
+  if (!['development', 'production'].includes(process.env.NODE_ENV))
+    throw `Unknown NODE_ENV: ${process.env.NODE_ENV}`;
+  if (process.env.NODE_ENV === 'development') return devConfig;
+  if (!process.env.CF_PAGES && !process.env.NETLIFY) return buildConfig;
+  if (process.env.CF_PAGES) return cloudflarePagesConfig;
+  if (process.env.NETLIFY) return netlifyConfig;
+  throw `Cannot select config`;
+}
+
+module.exports = selectConfig();
